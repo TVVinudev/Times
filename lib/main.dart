716c95e947b358313122a367ef010530fc50d8dc
Admin <dayanafrank2001@gmail.com>
2024-01-12 15:51:34 +0530

    initial Committ

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:time/firebase_options.dart';
+import 'package:time/read.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,29 +23,8 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: StreamBuilder(
-          stream: stu.snapshots(),
-          builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Object?>> snapshot) { 
-            if(snapshot.hasData){
-              return ListView.builder(
-                itemCount:snapshot.data!.docs.length,
-                itemBuilder: (BuildContext context, int index) { 
-                  final DocumentSnapshot snap =snapshot.data!.docs[index];
-                  return Column(
-                    children: [
-                      Text(snap['name']),
-                      Text(snap['number'].toString())
-                    ],
-                  );
-                },
-              );
-            }
-            return CircularProgressIndicator();
-          },
-          
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+     home: read(),
     );
   }
 }
--- lib/read.dart
+++ lib/read.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class read extends StatefulWidget {
@@ -6,10 +7,60 @@ class read extends StatefulWidget {
   @override
   State<read> createState() => _readState();
 }
+final CollectionReference stu = FirebaseFirestore.instance.collection('time');
 
 class _readState extends State<read> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('CRUD'),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: StreamBuilder(
+        stream: stu.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot<Object?>> snapshot) {
+          if(snapshot.hasData){
+            return ListView.builder(
+              itemCount:snapshot.data!.docs.length,
+              itemBuilder: (BuildContext context, int index) {
+                final DocumentSnapshot snap =snapshot.data!.docs[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    elevation: 7,
+                    child: ListTile(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      title:Text(snap['name']),
+                      subtitle:Text(snap['number'].toString()),
+                      trailing: Container(
+                        width: 200,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.edit),
+                            Icon(Icons.delete)
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            );
+          }
+          return CircularProgressIndicator();
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+        backgroundColor: Colors.lightGreen,
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
